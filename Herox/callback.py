7447661d98e60d401dbfc03585d209cc525d658f
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-06-09 21:47:08 +0530

    Add files via upload

---
+++ Herox/callback.py
@@ -0,0 +1,183 @@
+from SJM.queues import QUEUE
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from config import (
+    ASSISTANT_NAME,
+    BOT_NAME,
+    BOT_USERNAME,
+    GROUP_SUPPORT,
+    OWNER_NAME,
+    UPDATES_CHANNEL,
+)
+
+
+
+
+
+
+
+@Client.on_callback_query(filters.regex("cbmenu"))
+async def cbmenu(_, query: CallbackQuery):
+    if query.message.sender_chat:
+        return await query.answer("you're an Anonymous Admin !\n\n» revert back to user account from admin rights.")
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("💡 only admin with manage voice chats permission that can tap this button !", show_alert=True)
+    chat_id = query.message.chat.id
+    if chat_id in QUEUE:
+          await query.edit_message_text(
+              f"⚙️ **settings of** {query.message.chat.title}\n\n⏸ : pause stream\n▶️ : resume stream\n🔇 : mute userbot\n🔊 : unmute userbot\n⏹ : stop stream",
+              reply_markup=InlineKeyboardMarkup(
+                  [[
+                      InlineKeyboardButton("⏹", callback_data="cbstop"),
+                      InlineKeyboardButton("⏸", callback_data="cbpause"),
+                      InlineKeyboardButton("▶️", callback_data="cbresume"),
+                  ],[
+                      InlineKeyboardButton("🔇", callback_data="cbmute"),
+                      InlineKeyboardButton("🔊", callback_data="cbunmute"),
+                  ],[
+                      InlineKeyboardButton("🗑 Close", callback_data="cls")],
+                  ]
+             ),
+         )
+    else:
+        await query.answer("❌ nothing is currently streaming", show_alert=True)
+        
+        
+#start
+
+
+
+@Client.on_callback_query(filters.regex("cb_start"))
+async def cb_start(_, query: CallbackQuery):
+    await query.edit_message_text(
+       f"""ʜᴇʟʟᴏ [✨](https://telegra.ph//file/fbbaa3b61523e879f5c28.jpg) **ᴡᴇʟᴄᴏᴍᴇ [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**\n
+ **ɪ ᴄᴀɴ ᴘʟᴀʏ ᴍᴜsɪᴄ ɪɴ ɢʀᴏᴜᴘ ᴠɪᴅᴇᴏ ᴄᴀʟʟ !!**
+ **ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴘʀᴏᴍᴏᴛᴇ 💫**
+ **ғᴏʀ ᴀɴʏ ʜᴇʟᴘ ᴊᴏɪɴ [BETA SUPPORT](https://t.me/BETA_BOTSUPPORT)**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "⛓ Aᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ Gʀᴏᴜᴘ",
+                        url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                    )
+                ],
+                [InlineKeyboardButton(
+                    "• Cᴏᴍᴍᴀɴᴅs", callback_data="cb_cmd"),],
+                [
+                    InlineKeyboardButton("• Oᴡɴᴇʀ", url=f"https://t.me/{OWNER_NAME}"),
+                    InlineKeyboardButton("• OTHER BOTZ", url=f"https://t.me/beta_bot_updates/157"),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "• Sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{GROUP_SUPPORT}"
+                    ),
+                    InlineKeyboardButton(
+                        "• Uᴘᴅᴀᴛᴇs", url=f"https://t.me/{UPDATES_CHANNEL}"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "• WEBSITE •", url="https://betaofficialbot.blogspot.com"
+                    )
+                ],
+            ]
+        ),
+    )
+
+    
+    
+    
+    #Help command
+    
+    
+@Client.on_callback_query(filters.regex("cb_cmd"))
+async def cb_cmd(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""✨ **Hello !**
+» **ғᴏʀ ᴀɴʏ ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅ ᴄʟɪᴄᴋ ʙᴜᴛᴛᴏɴs 🔭 !**
+⚡ Powered by [★彡 JEOL 彡★](https://t.me/JP_Jeol_org)""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("sᴏᴍᴇ ʙᴀsɪᴄ ᴄᴏᴍᴍᴀɴᴅ", callback_data="cb_basic"),
+                    InlineKeyboardButton("sᴏᴍᴇ ᴀᴅᴠᴀɴᴄᴇ ᴄᴏᴍᴍᴀɴᴅs", callback_data="cb_advance"),
+                ],
+                [InlineKeyboardButton("sᴏᴍᴇ ғᴜɴ ᴄᴏᴍᴍᴀɴᴅ", callback_data="cb_fun")],
+               
+                [InlineKeyboardButton("ʙᴀᴄᴋ", callback_data="cb_start")],
+            ]
+        ),
+    )
+    
+@Client.on_callback_query(filters.regex("cb_basic"))
+async def cb_basic(_, query: CallbackQuery):
+    await query.edit_message_text(  
+        f"""𝙎𝙞𝙢𝙥𝙡𝙚 𝙘𝙤𝙢𝙢𝙖𝙣𝙙 
+        
+        
+•  `/play (song name)` 
+•  `/vplay (song name)` 
+•  `/vstream (song name)` 
+•  `/skip` - skip the current song
+•  `/end` - stop music play
+•  `/pause` - pause song play
+•  `/resume` - resume song play
+•  `/mute` - mute assistant in vc
+•  `/lyrics (song name)`
+
+⚡ Powered By [★彡 JEOL 彡★](https://t.me/JP_Jeol_org) .""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("ʙᴀᴄᴋ", callback_data="cb_cmd")]]
+        ),
+    )
+    
+    
+@Client.on_callback_query(filters.regex("cb_advance"))
+async def cb_advance(_, query: CallbackQuery):
+    await query.edit_message_text(    
+      f"""𝙀𝙭𝙩𝙧𝙖 𝙘𝙤𝙢𝙢𝙖𝙣𝙙𝙨
+• `/ping` pong !!
+• `/start` - Alive msg ~group 
+• `/id` - Find out your grp and your id // stickers id also
+• `/uptime` - 💻
+• `/rmd` clean all downloads
+• `/clean` - clear storage 
+
+⚡ Powered By [★彡 JEOL 彡★](https://t.me/JP_Jeol_org) .""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("ʙᴀᴄᴋ", callback_data="cb_cmd")]]
+        ),
+    )
+    
+    
+@Client.on_callback_query(filters.regex("cb_fun"))
+async def cb_fun(_, query: CallbackQuery):
+    await query.edit_message_text(  
+        f"""𝙁𝙪𝙣 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨
+• `/jeol` ✌🏻
+• `/beta`  😍
+• `/mkn`    ❤️
+• `/music`   🎵
+• `/vaazha` 🌱   
+
+⚡ Powered By [★彡 JEOL 彡★](https://t.me/JP_Jeol_org) .""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("ʙᴀᴄᴋ", callback_data="cb_cmd")]]
+        ),
+    )
+        
+
+    
+    
+    
+        
+
+
+@Client.on_callback_query(filters.regex("cls"))
+async def close(_, query: CallbackQuery):
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("ɴɪᴋᴀʟ ʙsᴅᴋ ᴛᴜ ᴀᴅᴍɪɴ ɴᴀʜɪ ʜᴀɪ ɢʀᴘ ᴋᴀ !", show_alert=True)
+    await query.message.delete()
---
+++ Herox/start.py
@@ -0,0 +1,151 @@
+from datetime import datetime
+from sys import version_info
+from time import time
+
+from config import (
+    ALIVE_IMG,
+    ALIVE_NAME,
+    BOT_NAME,
+    BOT_USERNAME,
+    GROUP_SUPPORT,
+    OWNER_NAME,
+    UPDATES_CHANNEL,
+)
+from SJM.decorators import sudo_users_only
+from SJM.filters import command
+from pyrogram import Client, filters
+from pyrogram import __version__ as pyrover
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+__major__ = 0
+__minor__ = 2
+__micro__ = 1
+
+__python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ("week", 60 * 60 * 24 * 7),
+    ("day", 60 * 60 * 24),
+    ("hour", 60 * 60),
+    ("min", 60),
+    ("sec", 1),
+)
+
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return "inf"
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
+    return ", ".join(parts)
+
+
+@Client.on_message(
+    command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.edited
+)
+async def start_private(client: Client, message: Message):
+ await message.reply_photo(
+        photo=f"https://telegra.ph//file/d6786d305f4962f6fb930.jpg",
+        caption=f"""ʜᴇʟʟᴏ✨ **ᴡᴇʟᴄᴏᴍᴇ {message.from_user.mention()} !**\n
+ **ɪ ᴄᴀɴ ᴘʟᴀʏ ᴍᴜsɪᴄ ɪɴ ɢʀᴏᴜᴘ ᴠɪᴅᴇᴏ ᴄᴀʟʟ !!**
+ **ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴘʀᴏᴍᴏᴛᴇ 💫**
+ **ғᴏʀ ᴀɴʏ ʜᴇʟᴘ ᴊᴏɪɴ [BETA SUPPORT](https://t.me/BETA_BOTSUPPORT)**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "⛓ Aᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ Gʀᴏᴜᴘ",
+                        url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                    )
+                ],
+                [InlineKeyboardButton(
+                    "• Cᴏᴍᴍᴀɴᴅs", callback_data="cb_cmd"),],
+                [
+                    InlineKeyboardButton("• Oᴡɴᴇʀ", url=f"https://t.me/{OWNER_NAME}"),
+                    InlineKeyboardButton("• OTHER BOTZ", url=f"https://t.me/beta_bot_updates/157"),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "• Sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{GROUP_SUPPORT}"
+                    ),
+                    InlineKeyboardButton(
+                        "• Uᴘᴅᴀᴛᴇs", url=f"https://t.me/{UPDATES_CHANNEL}"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "• WEBSITE •", url="http://www.betaofficialbot.blogspot.com/"
+                    )
+                ],
+            ]
+        ),
+    )
+
+
+@Client.on_message(
+    command(["start", f"start@{BOT_USERNAME}"]) & filters.group & ~filters.edited
+)
+async def start_group(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("• Sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{GROUP_SUPPORT}"),
+                InlineKeyboardButton(
+                    "• Uᴘᴅᴀᴛᴇs", url=f"https://t.me/{UPDATES_CHANNEL}"
+                ),
+            ]
+        ]
+    )
+
+    alive = f"**Hello {message.from_user.mention()}, i'm {BOT_NAME}**\n\n✨ Bot is working normally\n🍀 My Master: [{ALIVE_NAME}](https://t.me/{OWNER_NAME})\n🍀 Pyrogram Version: `{pyrover}`\n✨ Python Version: `{__python_version__}`\n🍀 Uptime Status: `{uptime}`\n\n**𝗧𝗵𝗮𝗻𝗸𝘀 𝗳𝗼𝗿 𝗔𝗱𝗱𝗶𝗻𝗴 𝗺𝗲 𝗵𝗲𝗿𝗲, 𝗳𝗼𝗿 𝗽𝗹𝗮𝘆𝗶𝗻𝗴 𝗺𝘂𝘀𝗶𝗰 𝗼𝗻 𝘆𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 𝘃𝗼𝗶𝗰𝗲 𝗰𝗵𝗮𝘁** ❤"
+
+    await message.reply_photo(
+        photo=f"{ALIVE_IMG}",
+        caption=alive,
+        reply_markup=keyboard,
+    )
+
+
+@Client.on_message(
+    command(["help", f"help@{BOT_USERNAME}"]) & filters.group & ~filters.edited
+)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        f"""✨ **Hello** {message.from_user.mention()} !
+» **press the button below to read the explanation and see the list of available commands !**
+⚡ __Powered by {BOT_NAME} A.I__""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="❓ Basic Guide", callback_data="cb_cmd")]]
+        ),
+    )
+
+
+@Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
+async def ping_pong(client: Client, message: Message):
+    start = time()
+    m_reply = await message.reply_text("pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text("🏓 Bot Alive 𝘽𝙀𝙏𝘼 𝙈𝙐𝙎𝙄𝘾 `PONG!!`\n" f"⚡️ `{delta_ping * 1000:.3f} ms`")
+
+
+@Client.on_message(command(["uptime", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
+@sudo_users_only
+async def get_uptime(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        "🤖 bot status:\n"
+        f"• **uptime:** `{uptime}`\n"
+        f"• **start time:** `{START_TIME_ISO}`"
+    )
