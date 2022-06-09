34afb6f9ba3590a7c3bd226766d0177cd79c3832
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-06-09 22:02:57 +0530

    Update play.py

--- Herox/play.py
+++ Herox/play.py
@@ -16,7 +16,7 @@ from PIL import Image, ImageDraw, ImageFont
 from config import ASSISTANT_NAME, BOT_USERNAME, IMG_1, IMG_2, IMG_5
 from SJM.filters import command, other_filters
 from SJM.queues import QUEUE, add_to_queue
-from TrickyAbhi.main import call_py, user
+from Beta.main import call_py, user
 from SJM.utils import bash
 from pyrogram import Client
 from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
