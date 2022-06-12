582e4e42ee03f635925cef6714316e980b33e89d
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-06-12 10:30:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# yooo guiz Herox 
+# powerd by Beta
 import os
 from os import getenv
 from dotenv import load_dotenv
@@ -15,18 +15,19 @@ API_ID = int(getenv("API_ID", "8945070"))
 API_HASH = getenv("API_HASH", "")
 OWNER_NAME = getenv("OWNER_NAME", "JP_Jeol_org")
 ALIVE_NAME = getenv("ALIVE_NAME", "JEOL")
-BOT_USERNAME = getenv("BOT_USERNAME", "SK_MUSIC_ROBOT")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "@OFFER_CLOSES_SOON")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "BETA_BOTSUPPORT")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "beta_bot_updates")
+BOT_USERNAME = getenv("BOT_USERNAME", "")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5372572893").split()))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph//file/69b6154eaecdaf3845d9f.jpg")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "70"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Jeolpaul/TrickyMusic")
+ALIVE_IMG = getenv("ALIVE_IMG", "")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/BETA-OFFICIAL/BETA-MUSICBOT")
 IMG_1 = getenv("IMG_1", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
 IMG_2 = getenv("IMG_2", "https://telegra.ph/file/596f75a52ea9bf0109644.png")
 IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
 IMG_4 = getenv("IMG_4", "https://telegra.ph/file/be5f551acb116292d15ec.png")
 IMG_5 = getenv("IMG_5", "https://telegra.ph/file/92e8c83e9148c6fea5f3b.png")
 IMG_6 = getenv("IMG_6", "https://telegra.ph/file/92e8c83e9148c6fea5f3b.png")
+DB_URL = getenv("DB_URL", "")
