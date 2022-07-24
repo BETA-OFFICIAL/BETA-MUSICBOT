4447da4514bb0e6214ad304bc157c1e6f5c35825
BETA BOTZ <102511853+Jeolpaul@users.noreply.github.com>
2022-07-24 11:59:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 from pytgcalls import idle
+
 from Beta.jeol import SUPPORT
 from Beta.main import call_py, bot, user as Herox
 
@@ -12,7 +13,7 @@ async def start_bot():
     print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
     await bot.join_chat("beta_bot_updates")
-    await Herox.send_message(SUPPORT, "<b>Congrats!! Music Bot has started successfully!</b>",
+    await Herox.send_message(f"@{LOG_USRNAME}, "<b>Congrats!! Music Bot has started successfully!</b>",
         )    
                           
 loop = asyncio.get_event_loop()
